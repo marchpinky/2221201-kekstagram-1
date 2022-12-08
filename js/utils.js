c9b527cc93ca989f69ff7933d072b772726c0639
Lana <pinkymarch696@gmail.com>
2022-12-08 20:19:39 +0500

    task 7/1

--- js/utils.js
+++ js/utils.js
@@ -3,7 +3,7 @@ const getRandomPositiveInteger = (a, b) => {
   const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
   const result = Math.random() * (upper - lower + 1) + lower;
   return Math.floor(result);
-}
+};
 
 const checkStringLength = (string, length) => string.length <= length;
 
